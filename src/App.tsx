5f8ff2e05dd10fd8b0da7d8f015c250af4221f6f
Brenden Chu <brenden@dokolabs.dev>
2024-08-29 09:30:03 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import UpvoteList from './components/UpvoteList'
+
+function App() {
+  return (
+    <>
+    <div className='container mx-auto p-8'>
+      <h1 className='font-bold text-2xl mb-4'>Upvote Demo</h1>
+      <div className='flex flex-col justify-start items-start gap-4'>
+      {[1, 2, 3].map((i) => (
+        <div className="flex justify-start w-full" key={i}>
+          <UpvoteList />
+        </div>
+      ))}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UpvoteList.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Upvote from "./Upvote";
+import UpvoteButton from "./UpvoteButton";
+import styles from './UpvoteList.module.css'
+
+export default function UpvoteList() {
+
+  const [numUpvotes, setNumUpvotes] = useState<number>(0)
+  const [selected, setSelected] = useState<number>(0)
+
+  const toggleSelection = (i: number) => () => {
+    setSelected(i)
+  }
+
+  const addUpvote = () => {
+    setNumUpvotes(numUpvotes + 1)
+    setSelected(numUpvotes)
+  }
+
+  return (
+    <>
+      <ul className={styles.list}>
+        {[...Array(numUpvotes)].map((_, i) => (
+          <li key={i}>
+            <Upvote 
+              selected={selected === i}
+              handler={toggleSelection(i)}
+             />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.button}>
+        <UpvoteButton handler={addUpvote} />
+      </div>
+    </>
+  )
+}
