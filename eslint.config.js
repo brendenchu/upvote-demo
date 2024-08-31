07a233b31eaaed822ec63c9548d0975300832fae
Brenden Chu <brenden@dokolabs.dev>
2024-08-30 17:45:06 -0700

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import prettier from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -17,11 +19,16 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    // Set the react version
+    settings: { react: { version: '18.3' } },
     plugins: {
+      react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
